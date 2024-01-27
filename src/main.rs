de7622202c9dcb1afdf1be6c6288b44c27a66cce
Martin M. Venturino <marventurino@gamil.com>
2024-01-27 14:16:18 -0300

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,22 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let firt_char = encoded_value.chars().next().unwrap();
+    if firt_char.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
+    }
+    else if firt_char == 'i' && encoded_value.chars().last().unwrap() == 'e' {
+        // Example: "i52e" -> "52"
+        let num_str = &encoded_value[1..=&encoded_value.len() - 2];
+        let num = num_str.parse::<i64>().unwrap();
+        return serde_json::Value::Number(num.into());
+    } 
+    else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
