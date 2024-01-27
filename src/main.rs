c0b5b373a8edd076dc3b1d90a1cfea811e2455f4
Martin M. Venturino <marventurino@gamil.com>
2024-01-27 12:40:36 -0300

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        //let number_string = &encoded_value[..colon_index];
+        //let number = number_string.parse::<i64>().unwrap();
+        let string = &encoded_value[(colon_index+1)..encoded_value.len()];
         return serde_json::Value::String(string.to_string());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
